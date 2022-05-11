8aa0f0fd5e7f5876c9f8dd1117413d1a76975cd9
srilakshmiprasanna@123 <srilakshmiprasanna@123.com>
2022-05-11 20:35:17 +0530

    Added Welcome Message

---
+++ src/main/java/com/example/demo/controller/HelloRestController.java
@@ -0,0 +1,37 @@
+package com.example.demo.controller;
+
+
+import com.example.demo.model.User;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloRestController {
+
+    @RequestMapping(value = {"", "/", "/home"})
+    public String sayHello() {
+        return "Hello From Bridgelabz !!!";
+    }
+
+    @RequestMapping(value = {"/query"}, method = RequestMethod.GET)
+    public String sayHello(@RequestParam(value = "name") String name) {
+        return "Hello" + name + "!";
+    }
+
+    @GetMapping("/param/{name}")
+    public String sayHelloParam(@PathVariable String name) {
+        return "Hello" + name + "!";
+    }
+
+    @PostMapping("/post")
+    public String sayHello(@RequestBody User user) {
+        return "Hello" + user.getFirstName() +" " + user.getLastName() + "!";
+    }
+
+    @PutMapping("/put/firstName")
+    public String sayHello(@PathVariable String firstName,
+                           @RequestParam(value = "lastName") String lastName) {
+        return "Hello" + firstName + " " + lastName + "!";
+    }
+
+}
